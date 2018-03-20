bd6ee3485505d7dfec92814152e0a19020ab92ad
Andrea M Rau <andrea.rau@inra.fr>
2018-03-20 15:54:06 -0500

    Decrease resolution of vignette

--- inst/doc/maskmeans.R
+++ inst/doc/maskmeans.R
@@ -12,7 +12,7 @@ X <- sim$data         ## Simulated data
 mv <- c(2,2,2,1,1,2)  ## Size of each view
 all.equal(sum(mv), ncol(X))
 
-## ---- fig.width = 10, fig.height=6---------------------------------------
+## ---- fig.width = 10, fig.height=6, dpi=36, out.width="600px", out.height="600px"----
 mv_plot(mv_data=X, mv=mv, labels=sim$labels[,1])
 
 ## ------------------------------------------------------------------------
@@ -24,11 +24,11 @@ split_hard_init <- kmeans(X[,1:2], centers=2)$cluster
 hard_aggreg <- maskmeans(mv_data=X, mv=mv, clustering_init=aggregate_hard_init, 
                          type = "aggregation", gamma=2) 
 
-## ---- fig.height = 8, fig.width=10---------------------------------------
+## ---- fig.height = 8, fig.width=10, dpi=36, out.width="600px", out.height="600px"----
 hard_aggreg$final_K
 p <- maskmeans_plot(hard_aggreg, mv_data = X)
 
-## ---- fig.width = 10, fig.height=6---------------------------------------
+## ---- fig.width = 10, fig.height=6, dpi=36, out.width="600px", out.height="600px"----
 # mv_plot(mv_data=X, mv=mv, labels=hard_aggreg$final_classification)
 adjustedRandIndex(hard_aggreg$final_classification, sim$labels[,1])
 adjustedRandIndex(aggregate_hard_init, sim$labels[,1])
@@ -37,11 +37,11 @@ adjustedRandIndex(aggregate_hard_init, sim$labels[,1])
 fuzzy_aggreg <- maskmeans(mv_data=X, mv=mv, clustering_init=aggregate_fuzzy_init, 
                          type = "aggregation", gamma=2) 
 
-## ---- fig.height = 8, fig.width=10---------------------------------------
+## ---- fig.height = 8, fig.width=10, dpi=36, out.width="600px", out.height="600px"----
 fuzzy_aggreg$final_K
 p <- maskmeans_plot(fuzzy_aggreg, mv_data = X)
 
-## ---- fig.width = 10, fig.height=6---------------------------------------
+## ---- fig.width = 10, fig.height=6, dpi=36, out.width="600px", out.height="600px"----
 # mv_plot(mv_data=X, mv=mv, labels=fuzzy_aggreg$final_classification)
 adjustedRandIndex(fuzzy_aggreg$final_classification, sim$labels[,1])
 adjustedRandIndex(apply(aggregate_fuzzy_init, 1, which.max), sim$labels[,1])
@@ -50,11 +50,11 @@ adjustedRandIndex(apply(aggregate_fuzzy_init, 1, which.max), sim$labels[,1])
 hard_split <- maskmeans(mv_data=X, mv=mv, clustering_init=split_hard_init, Kmax=20,
                          type = "splitting", gamma=2, perCluster_mv_weights=FALSE) 
 
-## ---- fig.height = 10, fig.width=10--------------------------------------
+## ---- fig.height = 10, fig.width=10, dpi=36, out.width="600px", out.height="600px"----
 hard_split$final_K
 p <- maskmeans_plot(hard_split, mv_data = X)
 
-## ---- fig.width = 10, fig.height=6---------------------------------------
+## ---- fig.width = 10, fig.height=6, dpi=36, out.width="600px", out.height="600px"----
 # mv_plot(mv_data=X, mv=mv, labels=hard_split$final_classification)
 adjustedRandIndex(hard_split$final_classification, sim$labels[,1])
 adjustedRandIndex(split_hard_init, sim$labels[,1])
@@ -63,14 +63,14 @@ adjustedRandIndex(split_hard_init, sim$labels[,1])
 hard_split_perCluster <- maskmeans(mv_data=X, mv=mv, clustering_init=split_hard_init, Kmax=20,
                          type = "splitting", gamma=2, perCluster_mv_weights=TRUE) 
 
-## ---- fig.height = 8, fig.width=10---------------------------------------
+## ---- fig.height = 8, fig.width=10, dpi=36, out.width="600px", out.height="600px"----
 hard_split_perCluster$final_K
 p <- maskmeans_plot(hard_split_perCluster, mv_data = X, type = c("criterion", "tree"))
 
-## ----  fig.height=8, fig.width=4-----------------------------------------
+## ----  fig.height=8, fig.width=4, dpi=36, out.width="600px", out.height="600px"----
 p <- maskmeans_plot(hard_split_perCluster, mv_data = X, type = c("weights"))
 
-## ---- fig.width = 10, fig.height=6---------------------------------------
+## ---- fig.width = 10, fig.height=6, dpi=36, out.width="600px", out.height="600px"----
 # v_plot(mv_data=X, mv=mv, labels=hard_split_perCluster$final_classification)
 adjustedRandIndex(hard_split_perCluster$final_classification, sim$labels[,1])
 adjustedRandIndex(split_hard_init, sim$labels[,1])
